29363ef5233c7ef02ed9af6d71bbbe3e75b9b823
Karthick <karthick212>
2021-05-01 21:48:47 +0530

    frst

--- server/config/db.js
+++ server/config/db.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
   // password: 'gowtham123',
   // database: 'gowtham'
 
-  host: 'gowthamnew.c25ghv7tlt4p.us-east-1.rds.amazonaws.com',
+  host: 'freelancetypers.cp6kqdu9boqh.us-east-1.rds.amazonaws.com',
   //host: 'donkeycargo.cn3up4gwqfwc.us-east-1.rds.amazonaws.com',
   user: 'gowtham',
   password: 'gowtham123',
